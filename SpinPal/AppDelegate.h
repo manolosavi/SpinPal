fef7d5c7676399f80e18dba99aca2b5213bfca07
manolo <manolosavi@me.com>
2015-03-19 19:01:20 -0600

    Initial commit.

---
+++ SpinPal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpinPal
+//
+//  Created by manolo on 3/19/15.
+//  Copyright (c) 2015 manolosavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpinPal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpinPal
+//
+//  Created by manolo on 3/19/15.
+//  Copyright (c) 2015 manolosavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
